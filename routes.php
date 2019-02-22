79036b085af5b9f5b75311d916110cb659b05695
Rafael Dias <rafael78@gmail.com>
2019-02-21 22:44:26 -0300

    getuserbyemail

--- routes.php
+++ routes.php
@@ -35,7 +35,7 @@ $app->group('/v1', function() {
     });
 
     $this->group('/getuserbyemail', function() {
-        $this->get('/{email:[.]+}', '\App\v1\Controllers\UserController:getUserByEmail');
+        $this->get('/{email:[a-zA-Z0-9_-@.]+}', '\App\v1\Controllers\UserController:getUserByEmail');
     });
 
     $this->group('/images', function() {
